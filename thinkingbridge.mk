73ef421ecccb7d4d4e82216694458d9f95728775
Adam77Root <adam77root@gmail.com>
2014-03-09 17:16:14 +0100

    ThinkingBridge support

---
+++ thinkingbridge.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2013 The OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/thinkingbridge/config/gsm.mk)
+
+# Inherit from the common AOSP product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/thinkingbridge/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/p880/p880.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := p880
+PRODUCT_NAME := thinkingbridge_p880
+PRODUCT_BRAND := LG
+PRODUCT_MODEL := LG-P880
+PRODUCT_MANUFACTURER := LGE
+
+$(call inherit-product-if-exists, vendor/lge/p880/p880-vendor.mk)
