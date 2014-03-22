791512f405c7d93c6d9e507c8cdf78eb0add564f
laufersteppenwolf <laufersteppenwolf@gmail.com>
2014-03-22 19:45:04 +0100

    Slim-ify the cm-11.0 branch

--- slim.mk
+++ slim.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -9,21 +9,20 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_RELEASE_NAME := p880
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # This device has NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/p880/p880.mk)
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := Optimus 4X HD
-#PRODUCT_VERSION_DEVICE_SPECIFIC :=
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p880
-PRODUCT_NAME := cm_p880
+PRODUCT_NAME := slim_p880
 PRODUCT_BRAND := LG
 PRODUCT_MODEL := LG-P880
 PRODUCT_MANUFACTURER := LGE
